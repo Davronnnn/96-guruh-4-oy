0d97a692a5e488e9e348959a77b33d4f5103c07b
Davronnn <kifleader@gmail.com>
2023-01-25 21:01:12 +0500

    last

--- js/test.js
+++ js/test.js
@@ -1,22 +1,56 @@
-// const getData = new Promise((resolve, reject) => {
-// 	if (false) {
-// 		resolve('Bajarildi');
-// 	} else {
-// 		reject('qaytarildi');
-// 	}
-// });
-// getData
-// 	.then((res) => {
-// 		console.log(res);
-// 	})
-// 	.catch((res) => {
-// 		console.log(res);
-// 	});
+const errorMessage = document.querySelector('#error-message');
+const elLoader = document.querySelector('.spinner-border');
+const elInput = document.querySelector('#input');
+const elCard = document.querySelector('.card');
+const title = elCard.querySelector('.card-title');
+const capital = elCard.querySelector('#capital');
+const flag = elCard.querySelector('#flag');
+const population = elCard.querySelector('#population');
+const region = elCard.querySelector('#region');
+const weather = elCard.querySelector('#weather');
 
-const data = fetch('https://jsonplaceholder.typicode.com/posts');
+const req = '';
 
-data.then((res) => {
-	return res.json();
-}).then((res) => {
-	console.log(res);
+fetch(`https://restcountries.com/v3.1/name/uzbekistan`)
+	.then((res) => res.json())
+	.then((data) => {
+		console.log(data);
+		title.textContent = data[0].name.common;
+		capital.textContent = data[0].capital;
+		flag.textContent = data[0].flag;
+		population.textContent += data[0].population;
+		region.textContent = data[0].subregion;
+	})
+	.catch((err) => {
+		title.textContent = 'Bu davlat mavjud emas';
+	});
+
+elInput.addEventListener('change', (e) => {
+	const value = e.target.value;
+
+	fetch(
+		`https://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&APPID=46351da790226c653537b9628dc20463`
+	)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+			weather.textContent = data.main.feels_like;
+		});
+	fetch(`https://restcountries.com/v3.1/name/${value}`)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+			title.textContent = data[0].name.common;
+			capital.textContent = data[0].capital;
+			flag.textContent = data[0].flag;
+			population.textContent = 'Aholisi:' + data[0].population;
+			region.textContent = data[0].subregion;
+		})
+		.catch((err) => {
+			title.textContent = 'Bu davlat mavjud emas';
+			capital.textContent = '';
+			flag.textContent = '';
+			population.textContent = '';
+			region.textContent = '';
+		});
 });
